27098b6189cacac326ec19c1daf9ff1ab4abed27
van11cab <60163011+van11cab@users.noreply.github.com>
2023-01-04 20:02:55 +0800

    Update project 2 try.py

--- project 2 try.py
+++ project 2 try.py
@@ -21,6 +21,8 @@ import numpy as np
 
 #get the max values of image
 #access the folder
+fwidth = 0
+fheight = 0
 imgdirectorry = os.listdir("C:/Users/Van Joseph/Documents/class/162/split images")
 for file in imgdirectorry:
     imgfilepath = "C:/Users/Van Joseph/Documents/class/162/split images/" + file
@@ -44,10 +46,10 @@ for file in imgdirectorry:
             if(currpixel[0] > max_imgR[x][y]):
                 max_imgR[x][y] = currpixel[0]
             #G
-            if(currpixel[1] > max_imgR[x][y]):
+            if(currpixel[1] > max_imgG[x][y]):
                 max_imgG[x][y] = currpixel[1]
             #B
-            if(currpixel[2] > max_imgR[x][y]):
+            if(currpixel[2] > max_imgB[x][y]):
                 max_imgB[x][y] = currpixel[2]
 
 
@@ -60,6 +62,7 @@ for file in imgdirectorry:
     image = Image.open(imgfilepath, 'r')
     image = image.convert("RGB")
     width, height = image.size
+    fwidth, fheight = image.size
     pixelmap = image.load()
 
     min_imgR = [[255] * height for _ in range(width)]
@@ -72,15 +75,40 @@ for file in imgdirectorry:
 
             #R
             if(currpixel[0] < min_imgR[x][y]):
-                max_imgR[x][y] = currpixel[0]
+                min_imgR[x][y] = currpixel[0]
             #G
-            if(currpixel[1] < min_imgR[x][y]):
-                max_imgG[x][y] = currpixel[1]
+            if(currpixel[1] < min_imgG[x][y]):
+                min_imgG[x][y] = currpixel[1]
             #B
-            if(currpixel[2] < min_imgR[x][y]):
-                max_imgB[x][y] = currpixel[2]
+            if(currpixel[2] < min_imgB[x][y]):
+                min_imgB[x][y] = currpixel[2]
 
+print(height)
+print(width)
+
+#lets create an image based on max and min
+import numpy as np
+import scipy.misc as smp
+
+#create an image size equal to width and height
+min_img = np.zeros((fwidth, fheight, 3), dtype = np.uint8)
+max_img = np.zeros((fwidth, fheight, 3), dtype = np.uint8)
+
+for x in range(0, fwidth):
+    for y in range(0, fheight):
+        holder= [min_imgR[x][y], min_imgG[x][y], min_imgB[x][y]]
+        min_img[x,y] = holder
+
+        holder= [max_imgR[x][y], max_imgG[x][y], max_imgB[x][y]]
+        max_img[x,y] = holder
+
+final_min = Image.fromarray(min_img)
+final_max = Image.fromarray(max_img)
+final_min.show()
+final_max.show()
 print("finih")
+
+
             
 
 
