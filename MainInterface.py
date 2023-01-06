4b117f253b3e6676ae851da6d7ef159463c22e7f
ferdi666 <ferdinandgadorii@gmail.com>
2023-01-06 08:10:53 +0800

    intial codes for the USER-interface

---
+++ MainInterface.py
@@ -0,0 +1,63 @@
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+from tkinter import ttk
+from PIL import Image, ImageTk
+import os
+
+
+imageList = []
+imageVariables = []
+
+def openFolder():
+    folderPath = filedialog.askdirectory()
+    imageFiles = os.listdir(folderPath)
+
+    for i in range(0, len(imageFiles)):
+        imageList.append([
+            ImageTk.PhotoImage(Image.open(folderPath+'/'+imageFiles[i]).resize((50,50))),
+            ImageTk.PhotoImage(Image.open(folderPath+'/'+imageFiles[i])),
+        ]),
+        imageVariables.append(f"img_{i}")
+
+    for i in range(len(imageVariables)):
+        globals()[imageVariables[i]] = tk.Button(imageSlider, image=imageList[i][0], bd=0, command=lambda i=i:displayImage(i))
+        globals()[imageVariables[i]].pack(side=tk.LEFT)
+
+    # print("Opening folder ...")
+
+def displayImage(index):
+    displayImageLabel.config(image=imageList[index][1])
+
+
+mainWindow = tk.Tk()
+mainWindow.title("CMSC 162 - Final Project")
+
+width = mainWindow.winfo_screenwidth()
+height = mainWindow.winfo_screenheight()
+mainWindow.geometry("%dx%d" % (width, height))
+
+mainMenuBar = tk.Menu(mainWindow)
+mainWindow.config(menu = mainMenuBar)
+
+fileMenu = tk.Menu(mainMenuBar, tearoff=0)
+fileMenu.add_command(label="Open Folder", command=openFolder)
+mainMenuBar.add_cascade(label="Functionalities", menu=fileMenu)
+
+displayImageLabel = tk.Label(mainWindow)
+displayImageLabel.pack(anchor=tk.CENTER)
+
+thumbnailCanvas = tk.Canvas(mainWindow, height=60)
+thumbnailCanvas.pack(side=tk.BOTTOM, fill=tk.X)
+
+xScrollBar = ttk.Scrollbar(mainWindow, orient=tk.HORIZONTAL)
+xScrollBar.pack(side=tk.BOTTOM, fill=tk.X)
+xScrollBar.config(command=thumbnailCanvas.xview)
+
+thumbnailCanvas.config(xscrollcommand=xScrollBar.set)
+thumbnailCanvas.bind("<Configure>", lambda e: thumbnailCanvas.bbox("all"))
+
+imageSlider = tk.Frame(thumbnailCanvas)
+thumbnailCanvas.create_window((0,0), window=imageSlider, anchor=tk.NW)
+
+mainWindow.mainloop()
